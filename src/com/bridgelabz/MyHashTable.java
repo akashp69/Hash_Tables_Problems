671a84b6143da8ecc38c2685df04945d0b0b7472
akashp69 <akashpakhare499@gmail.com>
2022-12-11 22:43:24 +0530

    Added: Program For Find the frequency of words present in paragraph

--- src/com/bridgelabz/MyHashTable.java
+++ src/com/bridgelabz/MyHashTable.java
@@ -1,24 +1,37 @@
 package com.bridgelabz;
+import java.util.ArrayList;
 
 public class MyHashTable<K, V> {
-    MyMapNode head;
-    MyMapNode tail;
+    MyMapNode<K, V> head;
+    MyMapNode<K, V> tail;
+
+    private final int numOfBuckets;
+    ArrayList<MyMapNode<K, V>> myBucketArray;
     /**
      * This Method is Used for to get the word from Linked List
      */
-
-    public V get(K word) {
-        MyMapNode<K, V> myNode = searchNode(word);
+    public MyHashTable() {
+        this.numOfBuckets = 10;
+        this.myBucketArray = new ArrayList<>(numOfBuckets);
+        /**
+         * Create empty LinkedLists
+         */
+        for (int i = 0; i < numOfBuckets; i++)
+            this.myBucketArray.add(null);
+    }
+    public V get(K key) {
+        int index = this.getBucketIndex(key);
+        if (this.myBucketArray.get(index) == null)
+            return null;
+        MyMapNode<K, V> myNode = search(key);
         return (myNode == null) ? null : myNode.getValue();
     }
     /**
      * This Method is Used for to search word from Linked List
      */
-    public MyMapNode<K, V> searchNode(K word) {
+    public MyMapNode<K, V> search(K word) {
         MyMapNode<K, V> currentNode = head;
-        int position = 0;
         while (currentNode != null) {
-            position++;
             if (currentNode.getKey().equals(word)) {
                 return currentNode;
             }
@@ -31,12 +44,25 @@ public class MyHashTable<K, V> {
      *  value: frequency of word.
      */
     public void add(K key, V value) {
-        MyMapNode<K, V> myNode = searchNode(key);
+        int index = this.getBucketIndex(key);
+        MyMapNode<K, V> myNode = this.myBucketArray.get(index);
+        if (myNode == null) {
+            myNode = new MyMapNode<>(key, value);
+            this.myBucketArray.set(index, myNode);
+        } else
+            myNode.setValue(value);
+        myNode = search(key);
         if (myNode == null) {
             myNode = new MyMapNode<>(key, value);
             this.append(myNode);
         } else
             myNode.setValue(value);
+    }
+    public int getBucketIndex(K key) {
+        int hashCode = Math.abs(key.hashCode());
+        int index = hashCode % numOfBuckets;
+        //       System.out.println("Key: "+key+" hashcode: "+hashCode+" index: "+index);
+        return index;
     }
     /**
      * This  Method is used  to append value to Linked List. myNode: value to append.
